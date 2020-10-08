471560898835411ef1990eac4dba72d9f9c04e23
Iapetus-11 <olimdahacker@gmail.com>
2020-10-08 09:25:04 -0400

    fix typos in example

--- examples/with commands.Bot outside a cog/bot.py
+++ examples/with commands.Bot outside a cog/bot.py
@@ -6,18 +6,18 @@ bot = commands.Bot(command_prefix='!')
 dbc = DisbotsClient(bot, 'secret', autopost_stats=True, webhook_port=5000, webhook_path='/disbots_hook')
 
 @bot.listen()
-async def on_disbots_test(self, data):
+async def on_disbots_test(data):
     print('A webhooks test occurred successfully! Data: ', data)
 
 @bot.listen()
-async def on_disbots_like(self, data):
+async def on_disbots_like(data):
     bot_id = data.bot_id  # bot id of the bot that was voted for
     user_id = data.user_id  # user id of the user who voted
 
     print(f'User {user_id} voted for bot {bot_id}')
 
 @bot.listen()
-async def on_ready(self):
+async def on_ready():
     print('Bot is ready and connected!')
 
 bot.run('discord token')
